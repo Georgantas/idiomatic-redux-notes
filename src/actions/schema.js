d4d9c359b83078bc92d455c2020019e619cbd348
Georgantas <john.georan@gmail.com>
2017-10-09 01:42:33 -0400

    progress

---
+++ src/actions/schema.js
@@ -0,0 +1,5 @@
+
+import { schema, arrayOf} from 'normalizr';
+
+export const todo = new schema.Entity('todos');
+export const arrayOfTodos = new schema.Array(todo);
--- src/reducers/byId.js
+++ src/reducers/byId.js
@@ -1,27 +1,34 @@
 
 
 const byId = (state = {}, action) => {
-    switch (action.type) {
-      case 'FETCH_TODOS_SUCCESS':
-        const nextState = { ...state };
-        action.response.forEach( todo => {
-          nextState[todo.id] = todo; // stays pure
-        });
-        return nextState;
-      // case 'ADD_TODO':
-      // case 'TOGGLE_TODO':
-      //   return {
-      //     ...state,
-      //     [action.id]: todo(state[action.id], action),
-      //   }
-      case 'ADD_TODO_SUCCESS':
-        return {
-          ...state,
-          [action.response.id]: action.response,
-        }
-      default:
-        return state;
+    // switch (action.type) {
+    //   case 'FETCH_TODOS_SUCCESS':
+    //     const nextState = { ...state };
+    //     action.response.forEach( todo => {
+    //       nextState[todo.id] = todo; // stays pure
+    //     });
+    //     return nextState;
+    //   // case 'ADD_TODO':
+    //   // case 'TOGGLE_TODO':
+    //   //   return {
+    //   //     ...state,
+    //   //     [action.id]: todo(state[action.id], action),
+    //   //   }
+    //   case 'ADD_TODO_SUCCESS':
+    //     return {
+    //       ...state,
+    //       [action.response.id]: action.response,
+    //     }
+    //   default:
+    //     return state;
+    // }
+    if(action.response){
+      return {
+        ...state,
+        ...action.response.entities.todos
+      }
     }
+    return state;
   };
 
   export default byId;
